ac3578dbbc4757cb4bfa04d6f09b5180dc3d58ac
YLeonardo <leonardguti10@gmail.com>
2023-05-26 03:05:04 -0600

    Updated login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/index.css';
 import AppNavbar from './Components/Navbar/index';
-import AppFooter from './Components/Footer/footer';
+import AppFooter from './Components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
