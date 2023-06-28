7180c95bd9d9bd7df3db15bb7b15dfc1cc80e4c8
Sherani VM <sheranivazquez@gmail.com>
2023-06-28 02:57:24 -0600

    Modificando agregar recepcionista

--- backend/controllers/recepcionistas.js
+++ backend/controllers/recepcionistas.js
@@ -1,24 +1,52 @@
 import { pool } from "../db.js";
-
-// Registrar un nuevo recepcionista
+//Falta el horario
 export const insertRecepcionista = async (req, res) => {
-  const { nombre, ap_paterno, ap_materno, usuario_id_usuario, horario_id_horario } = req.body;
+  const { nombre, ap_paterno, ap_materno, password, correo } = req.body;
 
-  if (!nombre || !ap_paterno || !ap_materno || !usuario_id_usuario || !horario_id_horario) {
-    return res.status(400).json({ message: "Faltan campos requeridos para registrar el recepcionista" });
+  // Verificar que todos los campos requeridos existan en el JSON
+  if (!nombre || !ap_paterno || !ap_materno || !password || !correo) {
+    return res.status(400).json({ message: "Faltan campos requeridos para insertar Recepcionista" });
   }
 
   try {
-    // Llamada al procedimiento almacenado
-    await pool.query("CALL sp_InsertarRecepcionista(?, ?, ?, ?, ?)", [
-      nombre,
-      ap_paterno,
-      ap_materno,
-      usuario_id_usuario,
-      horario_id_horario
-    ]);
+    // Verificar si el correo ya existe en la tabla db_usuario
+    const [existingUsuario] = await pool.query("SELECT id_usuario FROM db_usuario WHERE correo = ?", [correo]);
 
-    return res.status(200).json({ message: "Recepcionista registrado con éxito" });
+    console.log("Verifica el correo " [correo]);
+
+    if (existingUsuario.length > 0) {
+      return res.status(409).json({ message: "El correo ya está registrado" });
+    }
+
+    // Insertar al administrador en la tabla db_usuario
+    const tipoUsuarioId = 2; // ID para el tipo de usuario "Recepcionista"
+
+    console.log("Insertando el usuario en db_usuario");
+
+    const { insertId: usuarioId } = await pool.query(
+      "INSERT INTO db_usuario (tipo_usuario, correo, password) VALUES (?, ?, ?)",
+      [tipoUsuarioId, correo, password]
+    );
+    
+
+    const [usuarioIdCheck] = await pool.query("SELECT id_usuario FROM db_usuario WHERE correo = ?", [correo]);
+
+    console.log("Checa que el usuario exista", usuarioIdCheck);
+
+    const usuarioIdExtract = usuarioIdCheck[0].id_usuario;
+    const usuarioIdEntero = parseInt(usuarioIdExtract);
+
+    console.log("Id usuario entero", usuarioIdExtract);
+
+    // Insertar recepcionista en la tabla db_recepcionista
+    await pool.query(
+      "INSERT INTO db_recepcionista (nombre, ap_paterno, ap_materno, usuario_id_usuario) VALUES (?, ?, ?, ?)",
+      [nombre, ap_paterno, ap_materno, usuarioIdEntero]
+    );
+
+    console.log("Recepcionista agregado");
+
+    return res.status(201).json({ message: "Administrador creado con éxito" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
